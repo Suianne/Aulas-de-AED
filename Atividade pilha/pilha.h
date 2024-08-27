27e9a15a4ce602c692cabbdc2583f3cdbfc37cf0
Suianne Martins <martinssuianne@gmail.com>
2024-08-27 20:27:18 -0300

    Construção da bilbioteca
    essa parte da biblioteca tem as funções básicas de uma pilha de inteiros

--- Atividade pilha/pilha.h
+++ Atividade pilha/pilha.h
@@ -12,5 +12,41 @@ typedef struct {
 
 } type_stack;
 
+void initializeStack(type_stack *p){
+    p->top = -1; //para inicializar a pilha
+}
 
+int stackEmpty(type_stack *p){
+    if(p->top == -1) return 1; //aqui, 1 representa "true"
+    return 0; //aqui, o retorno do número 0 "false"
+}
+
+int fullStack(type_stack *p){
+    if(p->top == MAX-1){
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+int push(type_stack *p, type_item e){
+    if(stackEmpty(p)==1) return 0; //a pilha tá cheia e eu não posso empilhar mais
+    p->top++;
+    p->item[p->top] = e;
+    return 1;
+}
+
+int pop(type_stack *p, type_item *e){
+
+    if(stackEmpty(p)) //verifica se a pilha está vazia, se estiver vazia (1 - true), aborta e não retirar
+        return 0; 
+    *e = p->item[p->top];
+    return 1;
+}
+
+int top (type_stack *p, type_item *e){
+    if(stackEmpty(p)) return 0;
+    *e = p->item(p->top);
+    return 1;
+}
 #endif
